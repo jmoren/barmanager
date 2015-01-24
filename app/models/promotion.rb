9040b77dc0d5cb27d9bce70d011f325a3b0d40aa
Jorge Moreno <jmoren@gmail.com>
2015-01-23 22:22:45 -0300

    Validates promotions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,6 +1,10 @@
 class Promotion < ActiveRecord::Base
   has_many :promotion_items, dependent: :destroy
 
+  validates :description, :code, :day_price, :night_price, presence: true
+  validates :day_price, :night_price, numericality: { greater_than: 0 }
+  validates :code, uniqueness: true
+
   scope :favourites, -> { where(favourite: true) }
 
   def price
