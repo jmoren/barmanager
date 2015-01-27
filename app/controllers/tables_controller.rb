0206136b862fbf1772dbed76ad936c254c38e489
jairo de morais <jairodemorais@gmail.com>
2015-01-27 15:58:34 -0300

    add color to tables

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -98,6 +98,6 @@ class TablesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def table_params
-      params.require(:table).permit(:number, :description)
+      params.require(:table).permit(:number, :description, :color)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123172928) do
+ActiveRecord::Schema.define(version: 20150127173153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,10 +112,7 @@ ActiveRecord::Schema.define(version: 20150123172928) do
     t.datetime "updated_at"
     t.float    "closing_cash"
     t.float    "opening_cash"
-    t.integer  "user_id"
   end
-
-  add_index "shifts", ["user_id"], name: "index_shifts_on_user_id", using: :btree
 
   create_table "tables", force: true do |t|
     t.integer  "number"
@@ -123,6 +120,7 @@ ActiveRecord::Schema.define(version: 20150123172928) do
     t.datetime "updated_at"
     t.string   "status"
     t.string   "description"
+    t.string   "color",       default: "#27292B"
   end
 
   create_table "tickets", force: true do |t|
@@ -138,23 +136,5 @@ ActiveRecord::Schema.define(version: 20150123172928) do
   end
 
   add_index "tickets", ["shift_id"], name: "index_tickets_on_shift_id", using: :btree
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
