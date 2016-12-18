fcde0b45da94e55bab2f370d149030489d0945d0
Jairo de Morais <jairodemorais@gmail.com>
2016-12-18 20:41:29 -0300

    add indexs for promotion id

--- db/migrate/20161218223258_add_indexs_to_item_tickets.rb
+++ db/migrate/20161218223258_add_indexs_to_item_tickets.rb
@@ -2,10 +2,14 @@ class AddIndexsToItemTickets < ActiveRecord::Migration
   def up
     add_index(:item_tickets, :ticket_id)
     add_index(:item_tickets, :item_id)
+    add_index(:promotion_tickets, :ticket_id)
+    add_index(:promotion_tickets, :promotion_id)
   end
 
   def down
     remove_index(:item_tickets, :ticket_id)
     remove_index(:item_tickets, :item_id)
+    remove_index(:promotion_tickets, :ticket_id)
+    remove_index(:promotion_tickets, :promotion_id)
   end
 end
